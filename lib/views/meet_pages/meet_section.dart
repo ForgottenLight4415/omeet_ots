5b8964f91215fa31f5f5c7a56e70491ff5983064
crv44 <crv4415@gmail.com>
2022-07-27 19:44:04 +0530

    fix: meet url changed

--- lib/views/meet_pages/meet_section.dart
+++ lib/views/meet_pages/meet_section.dart
@@ -229,9 +229,9 @@ class _VideoMeetPageState extends State<VideoMeetPage> with AutomaticKeepAliveCl
         FeatureFlagEnum.LIVE_STREAMING_ENABLED: false,
         FeatureFlagEnum.RECORDING_ENABLED: false,
       };
-      final String meetId = widget.claim.claimNumber;
+      final String meetId = widget.claim.insuredContactNumber;
       var options = JitsiMeetingOptions(room: meetId)
-        ..serverURL = "https://hi.omeet.in/${widget.claim.insuredContactNumber}"
+        ..serverURL = "https://hi.omeet.in/$meetId"
         ..subject = "Meeting with ${widget.claim.insuredName}"
         ..userDisplayName = "OMeet Agent"
         ..userEmail = await AuthenticationProvider.getEmail()
@@ -239,7 +239,7 @@ class _VideoMeetPageState extends State<VideoMeetPage> with AutomaticKeepAliveCl
         ..audioMuted = _isAudioMuted
         ..videoMuted = _isVideoMuted
         ..featureFlags = featureFlags;
-
+      log(options.room);
       await JitsiMeet.joinMeeting(options, roomNameConstraints: {});
     } catch (error) {
       log(error.toString());
